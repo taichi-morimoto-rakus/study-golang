6af6787d3623288d409d0dd668b400da2162944b
Taichi Morimoto <taichi.morimoto@rakus.co.jp>
2025-03-14 15:33:46 +0900

    chapter05

--- tutorial/subject/chapter05/exam.go
+++ tutorial/subject/chapter05/exam.go
@@ -1,11 +1,33 @@
 package chapter05
 
 func DarumaDrop(daruma []int) []int {
-	// TODO: 実装
-	return []int{}
+	length := len(daruma)
+	if length == 0 || length == 1 {
+		return daruma
+	}
+
+	mid := length / 2
+	if length%2 == 0 {
+		if daruma[mid-1] >= daruma[mid] {
+			return append(daruma[:mid], daruma[mid+1:]...)
+		} else {
+			return append(daruma[:mid-1], daruma[mid:]...)
+		}
+	} else {
+		return append(daruma[:mid], daruma[mid+1:]...)
+	}
 }
 
 func MatrixMultiple(seed []int) [][]int {
-	// TODO: 実装
-	return [][]int{}
+	matrix := make([][]int, len(seed))
+	for i := range matrix {
+		matrix[i] = make([]int, len(seed))
+	}
+
+	for i, y := range seed {
+		for j, x := range seed {
+			matrix[i][j] = y * x
+		}
+	}
+	return matrix
 }
