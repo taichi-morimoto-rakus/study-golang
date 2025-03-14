580be8a0b73b6e5a76380599da98893c677c8079
Taichi Morimoto <taichi.morimoto@rakus.co.jp>
2025-03-14 17:35:34 +0900

    chapter08

--- tutorial/subject/chapter08/exam.go
+++ tutorial/subject/chapter08/exam.go
@@ -8,13 +8,44 @@ type Car interface {
 // Car を満たすように実装
 type Basic struct{}
 
+func (b Basic) PricePer15Minutes() int {
+	return 220
+}
+
+func (b Basic) MaxPrice() int {
+	return 4290
+}
+
 // Car を満たすように実装
 type Middle struct{}
+
+func (m Middle) PricePer15Minutes() int {
+	return 330
+}
+
+func (m Middle) MaxPrice() int {
+	return 6490
+}
 
 // Car を満たすように実装
 type Premium struct{}
 
+func (p Premium) PricePer15Minutes() int {
+	return 440
+}
+
+func (p Premium) MaxPrice() int {
+	return 8690
+}
+
 func Calc(car Car, minutes int) int {
-	// TODO: 実装
-	return 0
+	overMaxFeeMinutes := minutes % 360
+	overMaxFeeCount := minutes / 360
+	notIncludeMaxPrice := (overMaxFeeMinutes/15 + 1) * car.PricePer15Minutes()
+
+	if notIncludeMaxPrice >= car.MaxPrice() {
+		return car.MaxPrice() * (overMaxFeeCount + 1)
+	} else {
+		return notIncludeMaxPrice + car.MaxPrice()*overMaxFeeCount
+	}
 }
