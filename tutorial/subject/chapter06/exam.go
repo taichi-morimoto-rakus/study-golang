f7550d20fbf500633938032c13f3bc8607f32682
Taichi Morimoto <taichi.morimoto@rakus.co.jp>
2025-03-14 15:56:41 +0900

    chapter06

--- tutorial/subject/chapter06/exam.go
+++ tutorial/subject/chapter06/exam.go
@@ -2,6 +2,7 @@ package chapter06
 
 import (
 	"strconv"
+	"strings"
 
 	"tutorial/helper"
 )
@@ -17,11 +18,19 @@ func (n AhoNumber) aho() string {
 }
 
 func (n AhoNumber) Call() string {
-	// TODO: 実装
-	return ""
+	if n%3 == 0 || strings.Contains(strconv.Itoa(int(n)), "3") {
+		return n.aho()
+	} else {
+		return n.String()
+	}
 }
 
 func Nabeatsu(n int) []string {
-	// TODO: 実装
-	return []string{}
+	res := make([]string, 0, n)
+	for i := 1; i <= n; i++ {
+		aho := AhoNumber(i).Call()
+
+		res = append(res, aho)
+	}
+	return res
 }
